d762f313cc8126c39797728c0264a14eea703c84
Teo Klestrup Röijezon <teo@nullable.se>
2016-05-14 17:26:50 +0200

    Added a status message to the website + fixed push registration on first load

--- web-client/main.js
+++ web-client/main.js
@@ -1,13 +1,20 @@
+let msgBox = document.getElementById('msg');
+
 navigator.serviceWorker.register("worker.js").then(function(worker) {
   console.log("Registered worker:", worker);
+}).catch(function(err) {
+  console.log("Failed to register worker:", err);
+});
 
+navigator.serviceWorker.ready.then(function(worker) {
+  console.log("Worker ready! ", arguments);
   worker.pushManager.subscribe({
     "userVisibleOnly": true
   }).then(function(sub) {
     console.log("Subscription:", sub);
+    msgBox.innerText = "Successfully registered, endpoint: " + sub.endpoint;
   }).catch(function(err) {
     console.log("Failed to register push", err);
+    msgBox.innerText = "Failed to register, make sure push is allowed and check your logs";
   });
-}).catch(function(err) {
-  console.log("Failed to register worker:", err);
-})
+});
